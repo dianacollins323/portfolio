4b920b459b684437560a081ed089ad793dddf781
Diana Collins <dianacollins323@gmail.com>
2014-03-04 14:41:07 -0500

    css_animation cross browser compatible

--- javascript/global.js
+++ javascript/global.js
@@ -43,27 +43,27 @@ $( document ).ready(function() {
     //animated scroll to elements on page
     $("#indexLink").click(function() {
         target = $("#index");
-        $('body').animate({scrollTop: target.offset().top}, 600);
+        $('html,body').animate({scrollTop: target.offset().top}, 600);
     });
 
     $("#homeLink").click(function() {
         target = $("#index");
-        $('body').animate({scrollTop: target.offset().top}, 600);
+        $('html,body').animate({scrollTop: target.offset().top}, 600);
     });
 
     $("#portfolioLink").click(function() {
         target = $("#portfolio");
-        $('body').animate({scrollTop: target.offset().top}, 600);
+        $('html,body').animate({scrollTop: target.offset().top}, 600);
     });
 
     $("#resumeLink").click(function() {
         target = $("#resume");
-        $('body').animate({scrollTop: target.offset().top}, 600);
+        $('html,body').animate({scrollTop: target.offset().top}, 600);
     });
 
     $("#contactLink").click(function() {
         target = $("#contact");
-        $('body').animate({scrollTop: target.offset().top}, 600);
+        $('html,body').animate({scrollTop: target.offset().top}, 600);
     });
 
 });
